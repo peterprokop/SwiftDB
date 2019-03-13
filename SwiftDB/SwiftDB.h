24b0b2c4a2e466e0d00863d49f404e5a84fcab38
Pete <fatsomcfat@protonmail.com>
2019-03-13 14:37:58 +0000

    Move SwiftDB to separate framework

---
+++ SwiftDB/SwiftDB.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDB.h
+//  SwiftDB
+//
+//  Created by Pete Prokop on 13/03/2019.
+//  Copyright © 2019 Pete Prokop. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftDB.
+FOUNDATION_EXPORT double SwiftDBVersionNumber;
+
+//! Project version string for SwiftDB.
+FOUNDATION_EXPORT const unsigned char SwiftDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDB/PublicHeader.h>
+
+
